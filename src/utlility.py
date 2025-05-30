372a74cea3e213e598e1d8ba94670b2ad4ebe64e
NPF <froehlichnoah@t-online.de>
2025-05-31 00:16:46 +0200

    added Image,Link splitts

--- src/utlility.py
+++ src/utlility.py
@@ -30,3 +30,49 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        images = extract_markdown_images(text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        for image in images:
+            section = text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(section) != 2:
+                raise ValueError("invalid markdown")
+            if section[0] != "":
+                new_nodes.append(TextNode(section[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0],TextType.IMAGE,image[1]))
+            text = section[1]
+        if text != "":
+            new_nodes.append(TextNode(text,TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        images = extract_markdown_links(text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        for image in images:
+            section = text.split(f"[{image[0]}]({image[1]})", 1)
+            if len(section) != 2:
+                raise ValueError("invalid markdown")
+            if section[0] != "":
+                new_nodes.append(TextNode(section[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0],TextType.LINK,image[1]))
+            text = section[1]
+        if text != "":
+            new_nodes.append(TextNode(text,TextType.TEXT))
+    return new_nodes
