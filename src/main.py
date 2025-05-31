18bd04337ad090f3dda62e16a792483d028cf865
NPF <froehlichnoah@t-online.de>
2025-05-31 20:59:47 +0200

    added more pages

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from textnode import TextNode,TextType
+from copy import copy_static
+from website import generate_page,generate_page_recursive
+import os
 
+path_public = "./public"
+path_static = "./static"
+path_content = "./content"
+path_template = "./template.html"
 
 def main():
-    pass
+
+    copy_static(path_public,path_static)
+
+    generate_page_recursive(path_content,path_template,path_public)
 
 main()
